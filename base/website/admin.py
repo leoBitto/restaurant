f3dd40ff82ef28f7dad709a38fa9d5cc00c1aa46
leoBitto <leonardobitto@gmail.com>
2023-02-13 16:05:16 +0100

    added models wine, menu and dishes and admin pages for them

--- base/website/admin.py
+++ base/website/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
 from .models import *
 
+class Gallery_imageAdmin(admin.ModelAdmin):
+    list_display=('header', 'description', 'is_first', 'show_image')
+
+
+class DishAdmin(admin.ModelAdmin):
+    list_display=('name', 'price', 'description', 'type')
+    search_fields=("name", 'price')
+    list_filter=('type',)
+
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display=('id','is_lunch','pub_date')
+    list_filter=("is_lunch",)
+
+
+class WineAdmin(admin.ModelAdmin):
+    list_display=('name','cellar', 'year', 'description','price', 'in_our_cellar', )
+    list_filter=('cellar','year')
 
 
 # Register your models here.
-admin.site.register(Gallery_image)
-
+admin.site.register(Gallery_image, Gallery_imageAdmin)
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(Dish, DishAdmin)
+admin.site.register(Wine, WineAdmin)
+admin.site.register(Contact)
 
 
--- base/website/models.py
+++ base/website/models.py
@@ -1,7 +1,7 @@
 from django.db import models
+from django.utils.html import format_html
 
-
-# class gallery
+### information models
 class Gallery_image(models.Model):
     header = models.CharField(max_length=100, default="", blank=True, null=True)
     description = models.CharField(max_length=300, default="", blank=True, null=True)
@@ -11,3 +11,58 @@ class Gallery_image(models.Model):
     def __str__(self):
         return self.header
 
+    def show_image(self):
+        return format_html('<img src="{}" style="height:100px;" />', self.img.url)
+
+
+class Contact(models.Model):
+    phone = models.CharField(max_length=100, blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    mail = models.CharField(max_length=50, blank=True, null=True)
+
+
+
+### 
+class Dish(models.Model):
+
+    choices = (
+        (ENTREE := "ENTREE", 'Entree'),
+        (FIRST  := "FIRST", 'First'),
+        (SECOND  := "SECOND", 'Second'),
+        (SIDE  := "SIDE", 'Side'),
+        (SWEET  := "SWEET", 'Sweet'),
+    )
+        
+    name = models.CharField(max_length=100, null=True)
+    price = models.FloatField()
+    description = models.TextField()
+    type = models.CharField(max_length=10, choices=choices, default=ENTREE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Dishes'
+
+
+class Menu(models.Model):
+    is_lunch = models.BooleanField(default=False)
+    dishes = models.ManyToManyField(Dish)
+    pub_date = models.DateField(auto_now=True, editable=True)
+    
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name_plural = 'Menues'
+    
+
+class Wine(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    cellar = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField()
+    price = models.FloatField()
+    in_our_cellar = models.IntegerField(blank = True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}, {self.cellar}"
