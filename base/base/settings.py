c7b457924133ee4cad1f4758f304196ba77d0bf0
leoBitto <leonardobitto@gmail.com>
2023-03-02 14:43:06 +0100

    moved media folder in static folder

--- base/base/settings.py
+++ base/base/settings.py
@@ -150,10 +150,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # MEDIA
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
+
 
 
 # Default primary key field type
