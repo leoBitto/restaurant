b547fe5310273f826f16ecbe6b0ab29c4a928763
leoBitto <leonardobitto@gmail.com>
2023-03-02 18:59:03 +0100

    changed media root to staticfiles

--- base/base/settings.py
+++ base/base/settings.py
@@ -157,7 +157,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # MEDIA
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/media')
 
 
 
